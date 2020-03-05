0a6313be0521f4e809e6ae29ecafa8dc9ce84c8d
francisco marinho <franc1sc1566@gmail.com>
2020-03-05 17:15:55 -0300

    first commit

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-item
+    clickable
+    tag="a"
+    v-on:click="goToPage"
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'EssentialLink',
+  methods: {
+    goToPage () {
+      this.$router.push(this.pageRef)
+    }
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    caption: {
+      type: String,
+      default: ''
+    },
+
+    pageRef: {
+      String,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ src/pages/Persons.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page padding>
+    <ul>
+      <li v-for="person in persons" v-bind:key="person.id">
+        {{person}}
+      </li>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+  beforeMount () {
+    this.$axios.get('people', {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).then(result => {
+      this.persons = result.results
+    })
+  },
+
+  data: function () {
+    return {
+      persons: []
+    }
+  }
+}
+</script>
